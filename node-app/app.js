3a95cff29cddef007626fbfcc9d926e5180acfa9
Arvind Kumar Yadav <arvind.yadav@redBus.local>
2019-04-15 10:22:18 +0530

    app modification

--- node-app/app.js
+++ node-app/app.js
@@ -6,4 +6,17 @@ fs.appendFileSync('notes.txt','this file was created by the node js');
 fs.appendFileSync('notes.txt','My name is arvind kumar yadav')
 console.log(validator.isEmail("arvind.yadav@.com"));
 console.log(validator.isURL("http:/.gmail.com"))
-console.log(chalk.red.inverse.blue("this is my ball"))
+console.log(chalk.red.inverse.yellow("this is my ball"))
+
+
+const command= process.argv[2]
+if(command==='add')
+{
+    console.log("adding new note")
+}
+else if(command==='remove')
+{
+    console.log('removing note')
+}
+
+console.log(process.argv)
